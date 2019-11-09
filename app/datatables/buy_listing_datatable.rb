d44fae981218e790ec586dbc93df7b10b5e15647
Javier Morales <Javier Morales>
2019-11-09 13:22:07 -0300

    streamline buy and sell catalogs

--- app/datatables/buy_listing_datatable.rb
+++ app/datatables/buy_listing_datatable.rb
@@ -2,21 +2,16 @@ class BuyListingDatatable < AjaxDatatablesRails::ActiveRecord
 
   def view_columns
     @view_columns ||= {
-      card_thumbnail_uri: { source: "Card.thumbnail_uri", searchable: false, orderable: true },
       card_name:          { source: "Card.name", cond: :like, searchable: true, orderable: true },
-      set_name:           { source: "Card.set_name", cond: :like, searchable: true, orderable: true },
-      price:              { source: "BuyListing.price", searchable: false, orderable: true },
-      user:               { source: "BuyListing.user_id", searchable: false, orderable: true}
+      price:              { source: "BuyListing.price", searchable: false, orderable: true }
     }
   end
 
   def data
     records.map do |record|
       {
-        thumbnail_uri:  record.decorate.thumbnail_uri,
         card_name:      record.decorate.card_info,
         price:          record.decorate.price,
-        user:           record.decorate.user_info,
         DT_RowId:       record.id, # This will automagically set the id attribute on the corresponding <tr> in the datatable
       }
     end
--- app/decorators/buy_listing_decorator.rb
+++ app/decorators/buy_listing_decorator.rb
@@ -1,58 +1,64 @@
 class BuyListingDecorator < ApplicationDecorator
-  def thumbnail_uri
-    h.image_tag object.card.thumbnail_uri, class: 'w-full'
-  end
-
   def card_info
-    h.content_tag('span', "#{object.amount}x", class: 'text-sm text-gray-700 font-bold mr-1') +
-    h.link_to(
-      "#{object.card.name}",
-      "https://deckbox.org/mtg/#{URI.escape(object.card.name)}",
-      class: 'text-lg text-orange-600 cursor-pointer'
-    ) +
-    (object.foil ? h.content_tag('span', 'Foil', class: 'ml-1 text-sm uppercase font-bold tracking-wider') : '') +
     h.content_tag('div',
-      object.any_set ? "Edición buscada no especificada. Mostrando #{object.card.set_name}" : object.card.set_name,
-      class: 'text-xs italic') +
-    (object.notes ? h.content_tag('div', object.notes, class: 'text-sm') : '')
-  end
-
-  def user_info
-    if object.user.email == 'dsfoodandgames@gmail.com'
       h.content_tag('div',
+        h.content_tag('span', "#{object.amount}x", class: 'text-sm text-gray-700 font-bold mr-1 mb-8') +
+
         h.link_to(
-          h.image_tag('dragonstone.jpg', class: 'h-12 mr-2'),
-          "https://www.facebook.com/Dragonstonefoodygames/", target: "_blank"
-        ) +
+          "#{object.card.name}",
+          "https://shop.tcgplayer.com/magic/product/show?newSearch=false&IsProductNameExact=false&ProductName=#{object.card.name.gsub(" ", "+")}&orientation=list",
+        class: 'text-lg text-orange-600 cursor-pointer', target: '_blank') +
+        
+        (object.foil ? h.content_tag('span', 'Foil', class: 'ml-1 text-sm uppercase font-bold tracking-wider') : '') +
+        
         h.content_tag('div',
-          h.link_to('Dragon Stone', "https://www.facebook.com/Dragonstonefoodygames/", class: 'text-gray-600') +
-          h.content_tag('div', object.user.phone, class: 'text-gray-800 italic text-xs')
-        ),
-      class: 'flex items-center')
-    elsif h.current_user == object.user
-      h.content_tag('div', 'Yo')
-    else
-      h.content_tag('div', object.user.full_name, class: 'text-gray-600') +
-      h.content_tag('div', object.user.phone, class: 'text-gray-800 italic text-xs')
-    end
+          object.any_set ? "Edición buscada no especificada. Mostrando #{object.card.set_name}" : object.card.set_name,
+        class: 'text-xs italic'),
+      class: 'flex-1 mb-1 md:mt-4', style: 'min-width: 8rem') +
+      
+      h.image_tag(object.card.thumbnail_uri, class: 'w-full md:w-32 mr-4', style: 'max-width: 8rem'),
+    class: 'flex flex-wrap flex-row-reverse justify-end')
   end
 
   def price
     h.content_tag('div',
-      h.content_tag('span', 'USD', class: 'text-gray-700 text-sm font-bold') +
-      (
-        object.discount != 0 ?
-        h.content_tag('span', object.foil ? object.card.foil_price : object.card.price, class: 'text-base text-gray-500 line-through mx-1')
-        :
-        ''
-      ) +
-      h.content_tag('span', h.number_with_precision(object.price, precision: 2) || '?', class: 'text-gray-800 text-xl')
-    ) + 
-    h.link_to( 
-      'Ver detalle de precios en TCG',
-      "https://shop.tcgplayer.com/magic/product/show?newSearch=false&IsProductNameExact=false&ProductName=#{object.card.name.gsub(" ", "+")}&orientation=list",
-      class: "text-orange-600 hover:text-orange-400 cursor-pointer",
-      target: "_blank"
+      h.content_tag('div',
+        h.content_tag('span',
+          h.content_tag('div',
+            h.content_tag('span', 'USD ', class: 'text-gray-700 text-sm font-bold') +
+            h.content_tag('span', h.number_with_precision(object.price, precision: 2) || '?', class: 'text-gray-800 text-xl')
+          ) +
+          (
+            object.discount != 0 ?
+            h.content_tag('div', object.foil ? object.card.foil_price : object.card.price, class: 'text-sm text-gray-500 text-right line-through -mt-1')
+            :
+            ''
+          ),
+        class: 'inline-block'),
+      ) + 
+
+      (object.notes ? h.content_tag('div', object.notes, class: 'text-sm') : '') +
+
+
+      h.content_tag('div',
+        if object.user.email == 'dsfoodandgames@gmail.com'
+          h.content_tag('div',
+            h.link_to(
+              h.image_tag('dragonstone.jpg', class: 'h-12 mr-2'),
+              "https://www.facebook.com/Dragonstonefoodygames/", target: "_blank"
+            ) +
+            h.content_tag('div',
+              h.link_to('Dragon Stone', "https://www.facebook.com/Dragonstonefoodygames/", class: 'text-gray-600') +
+              h.content_tag('div', object.user.phone, class: 'text-gray-800 italic text-xs')
+            ),
+          class: 'flex items-center')
+        elsif h.current_user == object.user
+          h.content_tag('div', 'Yo')
+        else
+          h.content_tag('div', object.user.full_name, class: 'text-gray-600') +
+          h.content_tag('div', object.user.phone, class: 'text-gray-800 italic text-xs')
+        end,
+      class: 'mt-8'),
     )
   end
 end
